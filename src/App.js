3a553887720c6023e86453ff84f8d72bccfdd1ee
prem mokashe <prem123@gmail.com>
2021-01-06 14:21:23 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Fragment, useEffect } from 'react';
+import SearchBar from './component/layout/SearchBar';
+import Logs from './component/logs/Logs';
+import AddBtn from './component/layout/AddBtn'
+import AddLogModal from './component/logs/AddLogModal';
+import EditLogModal from './component/logs/EditLogModal';
+import AddTechModal from './component/techs/AddTechModal';
+import TechListModal from './component/techs/TechListModal';
+import { Provider } from 'react-redux';
+import store from './store';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
+
+const App = () => {
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  });
+  return (
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
+  );
+};
+
+export default App;
